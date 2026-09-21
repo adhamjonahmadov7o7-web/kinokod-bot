fb3f9cbe9e0015ea6c94106ae6e179b4829f2bb9
adhamjonahmadov7o7-web <adhamjonahmadov7o7@gmail.com>
2026-09-21 19:44:03 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Homiy kanallar ro'yxati (Kanal username'i yoki ID'si)
+  REQUIRED_CHANNELS: [
+    '@t.me/adhamjon_kino_kod'  // Keyinchalik 3-sida bo'lsa, vergul qo'yib qo'shasiz
+  ]
+};
