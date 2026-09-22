f727e7b5829b495f74a9322cc6f074adb214a046
adhamjonahmadov7o7-web <adhamjonahmadov7o7@gmail.com>
2026-09-22 16:24:44 +0500

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,5 +1,11 @@
-module.exports = [
-  "@Adhamjon_Live" // Kanalingiz manzili (t.me siz, shunchaki @ bilan)
-  // Keyinchalik yana kanal qo'shmoqchi bo'lsangiz, vergul qo'yib quyidagicha yozasiz:
-  // , "@ikkinchi_kanal"
-];
+module.exports = {
+  // Telegram kanallari (Username yoki Link)
+  telegramChannels: [
+    "@Adhamjon_Live" // Yoki privat kanal bo'lsa linkini qo'yasiz: "https://t.me/+AbCdEf..."
+  ],
+
+  // Instagram sahifalaringiz
+  instagramLinks: [
+    { name: "📸 Instagram sahifamiz", url: "https://instagram.com/rasmiy_sahifa" }
+  ]
+};
