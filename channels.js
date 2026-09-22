be76cabc45888c077c2ef34869fb5a46469a6a80
adhamjonahmadov7o7-web <adhamjonahmadov7o7@gmail.com>
2026-09-22 16:16:39 +0500

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,5 @@
+module.exports = [
+  "@Adhamjon_Live" // Kanalingiz manzili (t.me siz, shunchaki @ bilan)
+  // Keyinchalik yana kanal qo'shmoqchi bo'lsangiz, vergul qo'yib quyidagicha yozasiz:
+  // , "@ikkinchi_kanal"
+];
