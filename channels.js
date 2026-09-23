dd6f9907903ca7a6f5fadb1261e843258888b8b0
adhamjonahmadov7o7-web <adhamjonahmadov7o7@gmail.com>
2026-09-23 08:34:37 +0500

    Update channels.js

--- channels.js
+++ channels.js
@@ -4,8 +4,4 @@ module.exports = {
     "@Adhamjon_Live" // Yoki privat kanal bo'lsa linkini qo'yasiz: "https://t.me/+AbCdEf..."
   ],
 
-  // Instagram sahifalaringiz
-  instagramLinks: [
-    { name: "📸 Instagram sahifamiz", url: "https://instagram.com/rasmiy_sahifa" }
-  ]
-};
+ 
